62c2ce3c8fd9d03961d87747dcb5593eb4e603ea
Valentine <valentine.nazarov@gmail.com>
2021-06-04 19:56:14 +0300

    ffmpeg support

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+import ffmpeg
 
 
 def on_progress(stream, chunk, bytes_remaining):
@@ -8,7 +9,16 @@ def on_progress(stream, chunk, bytes_remaining):
 
 
 # video_url = input("Enter Youtube Video URL: ")
-video_url = "https://www.youtube.com/watch?v=vvpb8IdDZZI"
-youtube_obj = YouTube(video_url)
-youtube_obj.register_on_progress_callback(on_progress)
-youtube_obj.streams.filter(res='720p').first().download()
+video_url = "https://www.youtube.com/watch?v=xET1aTbiHno"
+youtube_obj = YouTube(video_url, on_progress_callback=on_progress)
+# streams = list(youtube_obj.streams)
+# print(*streams, sep='\n')
+# streams = list(youtube_obj.streams.filter(mime_type="video/mp4"))
+# print()
+# print(*streams, sep='\n')
+youtube_obj.streams.filter(file_extension='mp4', progressive=False).\
+    order_by('resolution').desc().first().download(filename='video')
+youtube_obj.streams.filter(mime_type="audio/mp4").first().download(filename='audio')
+video_stream = ffmpeg.input('video.mp4')
+audio_stream = ffmpeg.input('audio.mp4')
+ffmpeg.output(audio_stream, video_stream, 'out1.mp4', vcodec='copy', acodec='aac').run()
